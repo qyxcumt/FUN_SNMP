93547f1fee74b66c73105b408b54e42db097b89b
qyxcumt <qyxcumt@gmail.com>
2015-06-17 21:39:38 +0800

    没啥…只是新建了一个文件…困了…睡……

---
+++ SNMP/SNMP/SNMPCode.cpp
@@ -0,0 +1,27 @@
+/**
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©³©·¡¡¡¡¡¡©³©·
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡©³©¿©ß©¥©¥©¥©¿©ß©·
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§ ¡¡
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡©¥¡¡¡¡¡¡©§
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡£¾¡¡¡¡¡¡£¼¡¡©§
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§...¡¡¡Ð¡¡...¡¡©§
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡©»©¥©·¡¡¡¡¡¡©³©¥©¿
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡©§¡¡Code is far away from bug with the animal protecting¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡©§ ÉñÊÞ±£ÓÓ,´úÂëÎÞbug
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡©§¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡©§ ¡¡¡¡¡¡¡¡¡¡¡¡
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡©§
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡©§¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡©»©¥©¥©¥©·
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡¡¡¡¡¡¡¡¡©Ç©·
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§¡¡¡¡¡¡¡¡¡¡¡¡¡¡©³©¿
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©»©·©·©³©¥©×©·©³©¿
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©§©Ï©Ï¡¡©§©Ï©Ï
+* ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡©»©ß©¿¡¡©»©ß©¿
+*/
+
+#include <Windows.h>
+#include <WinSnmp.h>
+
