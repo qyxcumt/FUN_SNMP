bdd87df194417f1c6e8830b2e136ebbaa2ceb93c
qyxcumt <qyxcumt@gmail.com>
2015-06-28 23:25:43 +0800

    增加注释
    修改变量值转换成字符串方式，使用stringstream转换

--- SNMPTEST/SNMPTEST/main.cpp
+++ SNMPTEST/SNMPTEST/main.cpp
@@ -161,13 +161,13 @@ bool MySNMP::Send(const strvec& strOIDArray, smiINT sPDUType)
 	//创建实体
 	hSrcEntity = SnmpStrToEntity(hSession, "192.168.196.1");
 	hDestEntity = SnmpStrToEntity(hSession, IPAddr.c_str());
-
+	//设置实体超时时间
 	if (SnmpSetTimeout(hSrcEntity, 10) == SNMPAPI_FAILURE){
 		cout << GetLastError() << endl;
 		cout << "设置超时失败，在Send函数内,源" << endl;
 		return false;
 	}
-	//设置超时
+	//设置实体超时
 	if (SnmpSetTimeout(hDestEntity, 10) == SNMPAPI_FAILURE){
 		cout << "设置超时失败，在Send函数内,目的" << endl;
 		return false;
@@ -176,40 +176,41 @@ bool MySNMP::Send(const strvec& strOIDArray, smiINT sPDUType)
 	if (hSrcEntity == SNMPAPI_FAILURE || hDestEntity == SNMPAPI_FAILURE){
 		return false;
 	}
-	
+	//创建变量绑定表
 	if ((hVbl = SnmpCreateVbl(hSession, NULL, NULL)) == SNMPAPI_FAILURE){
 		return false;
 	}
-	
+	//绑定变量
 	for (strvec::const_iterator it = strOIDArray.begin(); it != strOIDArray.end(); it++){
-		SnmpStrToOid((*it).c_str(), &sOid);
-		SnmpSetVb(hVbl, 0, &sOid, NULL);
+		SnmpStrToOid((*it).c_str(), &sOid);		//OID字符串转换成smiOID类型
+		SnmpSetVb(hVbl, 0, &sOid, NULL);		//绑定变量
 	}
-
+	//设置PDU
 	if ((hPdu = SnmpCreatePdu(hSession, sPDUType, 0, NULL, NULL, hVbl)) == SNMPAPI_FAILURE){
 		return false;
 	}
-
+	//biu~biu~~biu~~~
 	if (SnmpSendMsg(hSession, hSrcEntity, hDestEntity, hContext, hPdu) == SNMPAPI_FAILURE){
 		return false;
 	}
-
+	//释放资源
 	SnmpFreePdu(hPdu);
 	return true;
 }
 
 bool MySNMP::Receive(HSNMP_VBL& hVbl)
 {
+	//等待Response报文——等待hEvent事件被触发
 	if (WaitForSingleObject(hEvent, 10000) == WAIT_TIMEOUT){
 		cout << "Wait time out" << endl;
 		return false;
 	}
-
+	//重置事件
 	ResetEvent(hEvent);
-
+	//今年过节不收礼啊~~~不收礼啊不收礼~~~收礼只收Response报文~~~
 	if (SnmpRecvMsg(hSession, &hSrcEntity, &hDestEntity, &hContext, &hPdu) == SNMPAPI_FAILURE)
 		return false;
-
+	//解析报文，获取PDU数据
 	smiINT PDU_type, error_status, error_index;
 	if (SnmpGetPduData(hPdu, &PDU_type, NULL, &error_status, &error_index, &hVbl) == SNMPAPI_FAILURE)
 		return false;
@@ -219,28 +220,30 @@ bool MySNMP::Receive(HSNMP_VBL& hVbl)
 
 bool MySNMP::GetValue(const string& strOID, string& strResult)
 {
+	//OID.0
 	strvec strOIDArray;
 	strOIDArray.push_back(strOID + ".0");
-
+	//发送Request报文
 	if (!Send(strOIDArray, SNMP_PDU_GET))
 		return false;
-
+	//接收报文
 	if (!Receive(hVbl))
 		return false;
 
 	SNMPAPI_STATUS iCount = SnmpCountVbl(hVbl);
-
+	//应该只有1个值
 	if (iCount != 1){
 		strResult = "SnmoCountVbl error";
 		return false;
 	}
+	//获取变量值
 	smiOID sOIDRecv;
 	smiVALUE sValue;
 	if (SnmpGetVb(hVbl, 1, &sOIDRecv, &sValue) == SNMPAPI_FAILURE){
 		strResult = "SnmpGetVb error";
 		return false;
 	}
-
+	//解析变量
 	strResult = ValueToString(sValue);
 	SnmpFreeVbl(hVbl);
 	return true;
@@ -248,57 +251,64 @@ bool MySNMP::GetValue(const string& strOID, string& strResult)
 
 bool MySNMP::GetTable(strvec strOIDArray, strvec_vec& strResultTable)
 {
+	//为空还查毛线
 	if (strOIDArray.empty())
 		return false;
-
+	//OID个数
 	size_t iOIDLen = count((strOIDArray[0].begin()), (strOIDArray[0].end()), '.') + 1;
 
 	while (true){
+		//发送get-next-request请求
 		if (!Send(strOIDArray, SNMP_PDU_GETNEXT))
 			return false;
-
+		//接收报文
 		if (!Receive(hVbl))
 			return false;
-
+		//变量个数
 		size_t iCount = SnmpCountVbl(hVbl);
 
 		smiOID sOIDRecv, sOIDSend;
 		smiVALUE sValue;
 		smiINT lIfEnd;
 		char* buff = new char[100];
-
+		//分别获取每个变量值
 		for (size_t i = 0; i < iCount; ++i){
 			if (SnmpGetVb(hVbl, i + 1, &sOIDRecv, &sValue) == SNMPAPI_FAILURE){
 				cout << "获取结果失败" << endl;
 				return false;
 			}
+			//下一个OID…表是否结束
 			memset(buff, 0, 100);
 			SnmpStrToOid(strOIDArray[i].c_str(), &sOIDSend);
 			SnmpOidToStr(&sOIDRecv, 100, buff);
 			strOIDArray[i] = string(buff);
 			SnmpOidCompare(&sOIDSend, &sOIDRecv, iOIDLen , &lIfEnd);
-
+			//结束，退出循环
 			if (lIfEnd != 0)
 				break;
-
+			//否则把变量值放入返回向量中
 			strResultTable[i].push_back(ValueToString(sValue));
 		}
 		if (lIfEnd != 0)
 			break;
 	}
-
+	//释放资源
 	SnmpFreeVbl(hVbl);
 	return true;
 }
 
 string MySNMP::ValueToString(smiVALUE sValue)
 {
+	stringstream ss;
 	switch (sValue.syntax){
 	case SNMP_SYNTAX_INT:{
+		/*
 		char* buff = new char[12];
 		_itoa_s(sValue.value.sNumber, buff, 12, 10);
 		string value(buff);
-		return value;
+		return value;*/
+		ss << sValue.value.sNumber;
+		break;
 	}
 	case SNMP_SYNTAX_CNTR32:
 	case SNMP_SYNTAX_GAUGE32:
@@ -308,8 +318,11 @@ string MySNMP::ValueToString(smiVALUE sValue)
 		_ultoa_s(sValue.value.uNumber, buff, 12, 10);
 		string value(buff);
 		return value;
+		ss << sValue.value.uNumber;
+		break;
 	}
 	case SNMP_SYNTAX_CNTR64:{
+		/*
 		char* buff = new char[22];
 		char* lbuff = new char[12];
 		_ultoa_s(sValue.value.hNumber.lopart, lbuff, 12, 10);
@@ -324,44 +337,59 @@ string MySNMP::ValueToString(smiVALUE sValue)
 		len = strlen(buff);
 		memcpy(buff + len, lbuff, 11);
 		string value(buff);
-		return value;
+		return value;*/
+		ss << sValue.value.hNumber.hipart << sValue.value.hNumber.lopart;
+		break;
 	}
-	case SNMP_SYNTAX_NSAPADDR:
 	case SNMP_SYNTAX_BITS:
 	case SNMP_SYNTAX_OPAQUE:
 	case SNMP_SYNTAX_OCTETS:{
 		char* buff = new char[sValue.value.string.len + 1];
 		memset(buff, 0, sValue.value.string.len + 1);
 		memcpy(buff, sValue.value.string.ptr, sValue.value.string.len);
-		string value(buff);
-		return value;
-	}/*
-	 case SNMP_SYNTAX_IPADDR:
-	 case SNMP_SYNTAX_NSAPADDR:{
-	 char* buff = new char[16];
-	 memset(buff, 0, 16);
-
-	 }*/
+		ss << buff;
+		break;
+	}
 	case SNMP_SYNTAX_OID:{
 		char* buff = new char[100];
 		memset(buff, 0, 100);
 		SnmpOidToStr(&sValue.value.oid, 99, buff);
-		string value(buff);
-		return value;
+		//string value(buff);
+		//return value;
+		ss << buff;
+		break;
 	}
 	case SNMP_SYNTAX_NULL:{
-		string value("NULL");
-		return value;
+		//string value("NULL");
+		//return value;
+		ss << "NULL";
+		break;
 	}
-	case SNMP_SYNTAX_IPADDR:
-		stringstream ss;
-		ss << (int)sValue.value.string.ptr[0] << "." 
+	case SNMP_SYNTAX_NOSUCHINSTANCE:{
+		ss << "no such instance";
+		break;
+	}
+	case SNMP_SYNTAX_NOSUCHOBJECT:{
+		ss << "no such object";
+		break;
+	}
+	case SNMP_SYNTAX_ENDOFMIBVIEW:{
+		ss << "end of MIB view";
+		break;
+	}
+	case SNMP_SYNTAX_NSAPADDR:
+	case SNMP_SYNTAX_IPADDR:{
+		ss << (int)sValue.value.string.ptr[0] << "."
 			<< (int)sValue.value.string.ptr[1] << "."
-			<< (int)sValue.value.string.ptr[2] << "." 
+			<< (int)sValue.value.string.ptr[2] << "."
 			<< (int)sValue.value.string.ptr[3];
-		string value(ss.str());
-		return value;
+		break;
 	}
+	default:
+		ss << "我也不知道是啥  " << "SyntaxValue:" << sValue.syntax;
+	}
+	string value(ss.str());
+	return value;
 }
 
 void MySNMP::GetSNMPInfo()
